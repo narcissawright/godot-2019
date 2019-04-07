56441c7bceae817144563f566f055475fc04d96d
Narcissa Wright <narcissawright@gmail.com>
2019-04-07 05:40:20 -0700

    bezier curve for tree

--- Narcissa 3.1/code/TreeGen.gd
+++ Narcissa 3.1/code/TreeGen.gd
@@ -11,39 +11,71 @@ var st = SurfaceTool.new()
 func _ready():
 	call_deferred("begin_generation")
 	
-func begin_generation():
+func first_attempt():
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	var root_origin = Vector3(0,0,0)
-	var v = Vector3.LEFT
 	var vertex_data = []
-	for i in range (6):
-		st.add_color(ColorN('brown'))
-		var new_v = v.rotated(Vector3.UP, i+1 * PI / 3)
-		var scatter = Vector3(randf(), 0, randf()).normalized() / 3
-		new_v += scatter
-		vertex_data.append(new_v)
-		st.add_vertex(new_v)
-	for i in range (6):
-		st.add_color(ColorN('brown'))
-		var new_v = vertex_data[i] * 0.8
-		new_v.y += 0.75
-		var scatter = Vector3(randf(), 0, randf()).normalized() / 3
-		new_v += scatter
-		st.add_vertex(new_v)
-
-	for i in range (6):
-		st.add_index(i)
-		st.add_index(i+6)
-		st.add_index(i+1)
-		if i != 5:
-			st.add_index(i+1)
-			st.add_index(i+6)
-			st.add_index(i+7)
-		else:
-			st.add_index(i)
-			st.add_index(i+1)
-			st.add_index(i-5)
+	var verts = 0
+	var faces = 0
+	var maxloop = 10
+	for k in range (maxloop):
+		for i in range (6):
+			st.add_color(ColorN('brown'))
+			var new_v
+			if vertex_data.size() >= 6:
+				new_v = vertex_data[i + (k-1)*6] * 0.85
+				var scatter = Vector3(randf()-0.5, 0, randf()-0.5).normalized() / (5 + k)
+				new_v += scatter
+			else:
+				new_v = Vector3.LEFT.rotated(Vector3.UP, i+1 * PI / 3)
+				var scatter = Vector3(randf()-0.5, 0, randf()-0.5).normalized() / 5
+				new_v += scatter
+			new_v.y = k
+			vertex_data.append(new_v)
+			st.add_vertex(new_v)
+			verts += 1
 			
+		if k != maxloop-1:
+			for i in range (6):
+				var j = i + (6*k);
+				st.add_index(j)
+				st.add_index(j+6)
+				st.add_index(j+1)
+				faces += 1
+				if i != 5:
+					st.add_index(j+1)
+					st.add_index(j+6)
+					st.add_index(j+7)
+					faces += 1
+				else:
+					st.add_index(j)
+					st.add_index(j+1)
+					st.add_index(j-5)
+					faces += 1
+					
+	print (verts)
+	print (faces)
+	done()
+	
+	
+func begin_generation():
+	var control_point_0 = Vector3(0,0,0)
+	var randx = (randf() - 0.5) * 4
+	var randz = (randf() - 0.5) * 4
+	var randy = 5 + (randf() * 5)
+	var control_point_1 = Vector3(randx,randy, randz)
+	randx = (randf() - 0.5) * 4
+	randz = (randf() - 0.5) * 4
+	var control_point_2 = Vector3(randx, 10, randz)
+	
+	st.begin(Mesh.PRIMITIVE_LINES)
+	var max_range = 10.0
+	for i in range (max_range):
+		var zero_one_interpolate = control_point_0.linear_interpolate(control_point_1, float(i)/max_range)
+		var one_two_interpolate = control_point_1.linear_interpolate(control_point_2, float(i)/max_range)
+		var bezier_point = zero_one_interpolate.linear_interpolate(one_two_interpolate, float(i)/max_range)
+		st.add_vertex(bezier_point)
+		if i != 0 and i != max_range-1:
+			st.add_vertex(bezier_point)
 	done()
 
 func initial_quad():
@@ -65,7 +97,11 @@ func extrude_down():
 		var v1 = data.vertex[v+1] - Vector3(0, 0.1, 0)
 		var v2 = data.vertex[v+2] - Vector3(0, 0.1, 0)
 		create_triangle(v0, v2, v1) # note the reversed winding order to make front face bottom
-	
+
+func _input(event):
+	if Input.is_action_pressed("q"):
+		begin_generation()
+
 func done():
 	st.generate_normals()
 	var arr_mesh = st.commit()
